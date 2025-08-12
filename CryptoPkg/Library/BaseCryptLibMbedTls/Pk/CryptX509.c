02314109cdc9d406eb0bcc3ce489d27138afcbc9
Ágatha Freitas <htafreit@gmail.com>
2025-08-12 15:53:36 -0300

    CryptoPkg/BaseCryptLibMbedTls: Fix wrong return in X509GetIssuerName
    Signed-off-by: Ágatha Freitas <htafreit@gmail.com>

--- CryptoPkg/Library/BaseCryptLibMbedTls/Pk/CryptX509.c
+++ CryptoPkg/Library/BaseCryptLibMbedTls/Pk/CryptX509.c
@@ -1125,7 +1125,6 @@ X509GetIssuerName (
 {
   mbedtls_x509_crt  Crt;
   INT32             Ret;
-  BOOLEAN           Status;
 
   if (Cert == NULL) {
     return FALSE;
@@ -1138,24 +1137,16 @@ X509GetIssuerName (
   Ret = mbedtls_x509_crt_parse_der (&Crt, Cert, CertSize);
 
   if (Ret == 0) {
-    if (*CertIssuerSize < Crt.serial.len) {
-      *CertIssuerSize = Crt.serial.len;
-      Status          = FALSE;
-      goto Cleanup;
-    }
-
     if (CertIssuer != NULL) {
-      CopyMem (CertIssuer, Crt.serial.p, Crt.serial.len);
+      CopyMem (CertIssuer, Crt.issuer_raw.p, Crt.issuer_raw.len);
     }
 
-    *CertIssuerSize = Crt.serial.len;
-    Status          = TRUE;
+    *CertIssuerSize = Crt.issuer_raw.len;
   }
 
-Cleanup:
   mbedtls_x509_crt_free (&Crt);
 
-  return Status;
+  return Ret == 0;
 }
 
 /**
